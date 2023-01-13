4b67a47b23ef36432d049ad135159989e267682c
ilman <ilman@restock.id>
2023-01-13 15:35:13 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,9 +30,6 @@ class AuthController extends Controller
 
     public function dsadasd()
     {
-        return "aaaa";
-    public function Conflict()
-    {
         return 'ini bakal konflik';
     }
 }
