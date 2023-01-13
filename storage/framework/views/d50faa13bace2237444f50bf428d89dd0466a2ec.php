59a7112d74daa8f6a38658954ee750a4626d8a90
ilman <ilman@restock.id>
2023-01-13 16:30:01 +0700

    Revert "Revert "Revert "Update AuthController.php"""
    This reverts commit c424869d38e801c1b1e758a52b5b11863cc2bda3.

--- storage/framework/views/d50faa13bace2237444f50bf428d89dd0466a2ec.php
+++
@@ -1,4 +0,0 @@
-<!-- JavaScripts -->
-<script src="<?php echo e(asset('assets/js/jquery-3.4.1.min.js')); ?>"></script>
-<script src="<?php echo e(asset('assets/js/plugins.min.js')); ?>"></script>
-<script src="<?php echo e(asset('assets/js/common.js')); ?>"></script><?php /**PATH C:\Users\Admin\Desktop\github-learn\resources\views/frontend/layouts/script.blade.php ENDPATH**/ ?>
